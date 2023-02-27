80f728d7cdd7616ffc0970b388fe4a0b3011f0e2
Yurbanis <yurbanisbriceno@gmail.com>
2023-02-27 02:49:52 +0000

    Se sube cambios del generador automatico de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cards = ["♦", "♥", "♠", "♣"];
+let numbers = ["2", "3", "4", "5"];
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  setInterval(displayCard, 1000);
 };
+
+function displayCard() {
+  let singleCards = cards[Math.floor(Math.random() * cards.length)];
+  let singleNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.getElementById("imagen1").innerHTML = singleCards;
+  document.getElementById("text").innerHTML = singleNumbers;
+  document.getElementById("imagen2").innerHTML = singleCards;
+}
